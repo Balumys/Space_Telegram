315e116ff0465e56fb342429dd538e6645f23754
Balumys <123511478+Balumys@users.noreply.github.com>
2023-02-08 22:06:40 +0300

    publish photo to Telegram channel

--- scr/telegram_bot.py
+++ scr/telegram_bot.py
@@ -3,13 +3,21 @@ from dotenv import load_dotenv
 import os
 
 
-def print_user_text_to_channel(text, token):
+def print_user_text_to_channel(channel, text, token):
     bot = telegram.Bot(token=token)
-    bot.send_message(text=text, chat_id="@spacechannel1906")
+    bot.send_message(text=text, chat_id=channel)
+
+
+def publish_user_image_to_channel(channel, img_path, token):
+    bot = telegram.Bot(token=token)
+    bot.send_photo(chat_id=channel, photo=open(img_path, "rb"))
 
 
 if __name__ == "__main__":
     load_dotenv()
     token = os.getenv("TELEGRAM_TOKEN")
     user_text = "Hello from Python"
-    print_user_text_to_channel(user_text, token)
+    tg_channel = "@spacechannel1906"
+    img_path = "/Users/sergeiperevera/PycharmProjects/Study/Space_Telegram/scr/Images/nasa_apod_7.jpg"
+    print_user_text_to_channel(tg_channel, user_text, token)
+    publish_user_image_to_channel(tg_channel, img_path, token)
