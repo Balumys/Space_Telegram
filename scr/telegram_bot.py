18cdea12923f7dd4dcf9c4f9c4617a9c2abebb1e
Balumys <123511478+Balumys@users.noreply.github.com>
2023-02-10 15:14:30 +0300

    Added with open

--- scr/telegram_bot.py
+++ scr/telegram_bot.py
@@ -3,4 +3,5 @@ import telegram
 
 def publish_user_image_to_channel(channel, img_path, token):
     bot = telegram.Bot(token=token)
-    bot.send_photo(chat_id=channel, photo=open(img_path, "rb"))
+    with open(img_path, "rb") as image:
+        bot.send_photo(chat_id=channel, photo=image)
